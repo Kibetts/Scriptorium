f5ee20988485ca8664d888b30d36dfe47ed42cb9
Brian Kibet <kibetsbrian@gmail.com>
2023-09-07 22:54:14 +0300

    Add books

--- app/seeds.py
+++ app/seeds.py
@@ -4,3 +4,13 @@ from datetime import datetime
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+user1 = User(name='Neema')
+user2 = User(name='Jabez')
+
+book1 = Book(title='To Kill a Mockingbird', author='Harper Lee', copies=5, created_at=datetime.now(), available=True)
+book2 = Book(title='The Great Gatsby', author='F. Scott Fitzgerald', copies=9, created_at=datetime.now(), available=True)
+book3 = Book(title='Pride and Prejudice', author='Jane Austen', copies=3, created_at=datetime.now(), available=True) 
+book4 = Book(title='1984', author='George Orwell', copies=9, created_at=datetime.now(), available=True)
+book5 = Book(title='The Catcher in the Rye', author='J.D. Salinger', copies=2, created_at=datetime.now(), available=True)
+book6 = Book(title='The Lord of the Rings', author='J.R.R. Tolkien', copies=6, created_at=datetime.now(), available=True)
