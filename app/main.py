79c71307c2dc250f97005e63d18afa2b709d375b
Brian Kibet <kibetsbrian@gmail.com>
2023-09-07 21:39:43 +0300

    define a vailable books function

--- app/main.py
+++ app/main.py
@@ -1,3 +1,14 @@
 from sqlalchemy.orm import sessionmaker
 from models import engine, User, Book, Request, Borrow
 from datetime import datetime, timedelta
+
+# Function to list available books and prompt user for input
+def list_available_books(session):
+    available_books = session.query(Book).filter(Book.available == True).all()
+
+    if not available_books:
+        print("No available books at the moment.")
+    else:
+        print("Available books:")
+        for book in available_books:
+            print(f"{book.book_id}: {book.title} by {book.author}")
