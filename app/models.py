e79356869b6cac544d3972c1acd20bee5e6df2c7
Brian Kibet <kibetsbrian@gmail.com>
2023-09-07 21:11:32 +0300

    create engine and User class

--- app/models.py
+++ app/models.py
@@ -3,3 +3,12 @@
 from sqlalchemy import create_engine, Column, Integer, String, Boolean, TIMESTAMP, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite:///scriptorium_base.db')
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    user_id = Column(Integer, primary_key=True)
+    name = Column(String)
