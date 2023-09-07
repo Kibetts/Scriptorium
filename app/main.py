d4d22964377e01130f533c3397ef94a68dfecb84
Brian Kibet <kibetsbrian@gmail.com>
2023-09-07 23:21:48 +0300

    make changes to database

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+
 from sqlalchemy.orm import sessionmaker
 from models import engine, User, Book, Request, Borrow
 from datetime import datetime, timedelta
@@ -118,7 +119,7 @@ if __name__ == "__main__":
 
     session.close()
 
-    
+
 
 
 
