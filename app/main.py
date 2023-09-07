b523875ef463085d90d3e0eb7557f03afa7ab6da
Brian Kibet <kibetsbrian@gmail.com>
2023-09-07 21:41:54 +0300

    create user input option

--- app/main.py
+++ app/main.py
@@ -12,3 +12,12 @@ def list_available_books(session):
         print("Available books:")
         for book in available_books:
             print(f"{book.book_id}: {book.title} by {book.author}")
+
+    user_input = input("Enter the title of the book you want to borrow or 'r' to request: ")
+
+    if user_input == 'r':
+         request_book(session)
+       
+    else:
+        book_title = user_input
+        borrow_book(session, book_title)
